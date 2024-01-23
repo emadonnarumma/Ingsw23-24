a3738d022b60b60d6989ec9181bd3154a067ba31
emadonnarumma <ema.donnarumma@studenti.unina.it>
2024-01-23 18:02:53 +0100

    updating Profile code

--- Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/profile/ProfileFragment.java
+++ Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/profile/ProfileFragment.java
@@ -4,30 +4,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.ingsw.dietiDeals24.databinding.FragmentProfileBinding;
+import com.ingsw.dietiDeals24.R;
 
 public class ProfileFragment extends Fragment {
-    private FragmentProfileBinding binding;
+    private Switch sellerSwitch;
+    private TextView sellerSwitchTextView;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textProfile;
-        return root;
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sellerSwitch = view.findViewById(R.id.seller_switch_profile);
+        sellerSwitchTextView = view.findViewById(R.id.seller_switch_text_profile);
+        sellerSwitch.setOnClickListener(v -> {
+            if (sellerSwitch.isChecked()) {
+                sellerSwitchTextView.setTextColor(getResources().getColor(R.color.green));
+
+                // Load the animation from the XML file
+                Animation animation = AnimationUtils.loadAnimation(requireActivity(), R.anim.switch_button_on);
+
+                // Start the animation on the TextView
+                sellerSwitchTextView.startAnimation(animation);
+            } else {
+                sellerSwitchTextView.setTextColor(getResources().getColor(R.color.grey));
+
+                // Load the animation from the XML file
+                Animation animation = AnimationUtils.loadAnimation(requireActivity(), R.anim.switch_button_off);
+
+                // Start the animation on the TextView
+                sellerSwitchTextView.startAnimation(animation);
+            }
+        });
+    }
+
+
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }

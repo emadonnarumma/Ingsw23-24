99446fbbb0a83c1b305b8d364680168caee5730d
HodNet <jonathanborrelli2001@gmail.com>
2024-02-07 17:44:58 +0100

    frontend: finendo editProfileFragment

---
+++ Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/profile/EditRegionFragment.java
@@ -0,0 +1,43 @@
+package com.ingsw.dietiDeals24.ui.home.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.ingsw.dietiDeals24.R;
+
+public class EditRegionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_edit_region, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setBackButtonEnabled(true);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        setBackButtonEnabled(false);
+    }
+
+    private void setBackButtonEnabled(boolean enabled) {
+        if (getActivity() instanceof AppCompatActivity) {
+            ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(enabled);
+            }
+        }
+    }
+}

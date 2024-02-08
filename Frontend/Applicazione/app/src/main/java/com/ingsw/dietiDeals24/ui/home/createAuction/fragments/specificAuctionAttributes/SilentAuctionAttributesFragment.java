6ce1859118f331ddd0690b2646fbccee0934ab74
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2024-02-08 17:29:01 +0100

    Frontend: modificato SilentAuctionAttributes
    Adesso il fragment è quasi completato ma l'asta non si può ancora creare

--- Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/createAuction/fragments/specificAuctionAttributes/SilentAuctionAttributesFragment.java
+++ Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/createAuction/fragments/specificAuctionAttributes/SilentAuctionAttributesFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,18 +15,73 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.ingsw.dietiDeals24.R;
+import com.ingsw.dietiDeals24.ui.home.createAuction.fragments.generalAuctionAttributes.GeneralAuctionAttributesViewModel;
 import com.ingsw.dietiDeals24.ui.utility.DecimalInputFilter;
 import com.ingsw.dietiDeals24.ui.utility.KeyboardFocusManager;
+import com.ingsw.dietiDeals24.ui.utility.auctionHolder.AuctionHolder;
 import com.stepstone.stepper.BlockingStep;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wx.wheelview.adapter.ArrayWheelAdapter;
+import com.wx.wheelview.widget.WheelView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class SilentAuctionAttributesFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     private TextView dateTextView;
+    private WheelView<String> minutesWheelView, hoursWheelView, daysWheelView, monthsWheelView;
+
+    private WheelView.WheelViewStyle wheelViewStyle;
+
+    private List<String> hourList, minuteList, dayList, monthList;
+
+    private GeneralAuctionAttributesViewModel viewModel;
+
+    private AuctionHolder genericAuctionAttributesHolder;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(requireActivity()).get(GeneralAuctionAttributesViewModel.class);
+        genericAuctionAttributesHolder = viewModel.getNewAuction().getValue();
+        setupLists();
+        setupWheelViewStyle();
+    }
+
+    private void setupWheelViewStyle() {
+        wheelViewStyle = new WheelView.WheelViewStyle();
+        wheelViewStyle.selectedTextColor = getResources().getColor(R.color.blue);
+        wheelViewStyle.textColor = getResources().getColor(R.color.gray);
+        wheelViewStyle.backgroundColor = getResources().getColor(R.color.white);
+        wheelViewStyle.selectedTextSize = 20;
+    }
+
+    private void setupLists() {
+        hourList = new ArrayList<>();
+        for (int i = 0; i <= 24; i++) {
+            hourList.add(String.valueOf(i));
+
+        }
+
+        minuteList = new ArrayList<>();
+        for (int i = 0; i <= 60; i++) {
+            minuteList.add(String.valueOf(i));
+        }
+
+        dayList = new ArrayList<>();
+        for (int i = 0; i <= 31; i++) {
+            dayList.add(String.valueOf(i));
+        }
+
+        monthList = new ArrayList<>();
+        for (int i = 0; i <= 12; i++) {
+            monthList.add(String.valueOf(i));
+        }
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,6 +92,71 @@ public class SilentAuctionAttributesFragment extends Fragment implements DatePic
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setupDatePicker(view);
+        setupWheelViews(view);
+    }
+
+    private void setupWheelViews(View view) {
+        setupMinutesWheelView(view);
+        setupHoursWheel(view);
+        setupDaysWheelView(view);
+        setupMonthsWheelView(view);
+    }
+
+    private void setupMinutesWheelView(@NonNull View view) {
+        minutesWheelView = view.findViewById(R.id.minutes_wheel_view_silent_auction_attributes);
+        minutesWheelView.setWheelAdapter(new ArrayWheelAdapter(getContext()));
+        minutesWheelView.setSkin(WheelView.Skin.Holo);
+        minutesWheelView.setStyle(wheelViewStyle);
+        minutesWheelView.setWheelData(minuteList);
+        minutesWheelView.setExtraText("Minuto/i", getResources().getColor(R.color.blue), 40, 100);
+        minutesWheelView.setOnWheelItemSelectedListener((position, data) -> updateDecrementTimeTextView());
+    }
+
+    private void setupHoursWheel(@NonNull View view) {
+        hoursWheelView = view.findViewById(R.id.hours_wheel_view_silent_auction_attributes);
+        hoursWheelView.setWheelAdapter(new ArrayWheelAdapter(getContext()));
+        hoursWheelView.setSkin(WheelView.Skin.Holo);
+        hoursWheelView.setStyle(wheelViewStyle);
+        hoursWheelView.setWheelData(hourList);
+        hoursWheelView.setExtraText("Ora/e", getResources().getColor(R.color.blue), 40, 100);
+        hoursWheelView.setSelection(1);
+        hoursWheelView.setOnWheelItemSelectedListener((position, data) -> updateDecrementTimeTextView());
+    }
+
+    private void setupDaysWheelView(View view) {
+        daysWheelView = view.findViewById(R.id.days_wheel_view_silent_auction_attributes);
+        daysWheelView.setWheelAdapter(new ArrayWheelAdapter(getContext()));
+        daysWheelView.setSkin(WheelView.Skin.Holo);
+        daysWheelView.setStyle(wheelViewStyle);
+        daysWheelView.setWheelData(dayList);
+        daysWheelView.setExtraText("Giorno/i", getResources().getColor(R.color.blue), 40, 100);
+        daysWheelView.setOnWheelItemSelectedListener((position, data) -> updateDecrementTimeTextView());
+    }
+
+    private void setupMonthsWheelView(View view) {
+        monthsWheelView = view.findViewById(R.id.months_wheel_view_silent_auction_attributes);
+        monthsWheelView.setWheelAdapter(new ArrayWheelAdapter(getContext()));
+        monthsWheelView.setSkin(WheelView.Skin.Holo);
+        monthsWheelView.setStyle(wheelViewStyle);
+        monthsWheelView.setWheelData(monthList);
+        monthsWheelView.setExtraText("Mese/i", getResources().getColor(R.color.blue), 40, 100);
+        monthsWheelView.setOnWheelItemSelectedListener((position, data) -> updateDecrementTimeTextView());
+    }
+
+    private void updateDecrementTimeTextView() {
+        String decrementTime = "Decremento ogni: ";
+        if (!monthsWheelView.getSelectionItem().equals("0")) {
+            decrementTime += monthsWheelView.getSelectionItem() + "M ";
+        }
+        if (!daysWheelView.getSelectionItem().equals("0")) {
+            decrementTime += daysWheelView.getSelectionItem() + "G ";
+        }
+        if (!hoursWheelView.getSelectionItem().equals("0")) {
+            decrementTime += hoursWheelView.getSelectionItem() + "H ";
+        }
+        if (!minutesWheelView.getSelectionItem().equals("0")) {
+            decrementTime += minutesWheelView.getSelectionItem() + "Min";
+        }
     }
 
     private void setupDatePicker(View view) {

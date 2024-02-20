4b8e648a2b8889726a9a72ec0731a3cd902df21c
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2024-02-20 16:12:35 +0100

    Update SuccessfullSilentAuctionActivity.java

--- Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/myAuctions/auctionDetails/silentAuction/SuccessfullSilentAuctionActivity.java
+++ Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/myAuctions/auctionDetails/silentAuction/SuccessfullSilentAuctionActivity.java
@@ -3,7 +3,11 @@ package com.ingsw.dietiDeals24.ui.home.myAuctions.auctionDetails.silentAuction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.ViewGroup;
 
+import androidx.appcompat.content.res.AppCompatResources;
+
+import com.ingsw.dietiDeals24.R;
 import com.ingsw.dietiDeals24.controller.MyAuctionDetailsController;
 import com.ingsw.dietiDeals24.model.SilentAuction;
 import com.ingsw.dietiDeals24.model.enumeration.AuctionStatus;
@@ -45,6 +49,11 @@ public class SuccessfullSilentAuctionActivity extends AuctionDetailsActivity {
         descriptionTextViewAuctionDetails.setText(auction.getDescription());
         specificInformation1TextViewAuctionDetails.setText("scade il: " + auction.getExpirationDate());
         specificInformation2TextViewAuctionDetails.setText(MyAuctionDetailsController.getWithdrawalTimeText(auction));
+        redButton.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+        redButton.requestLayout();
+        redButton.setBackground(AppCompatResources.getDrawable(this, R.drawable.square_shape_red));
+        redButton.setText("RIMUOVI");
+
     }
 
     private void bindImages(SilentAuction silentAuction) {

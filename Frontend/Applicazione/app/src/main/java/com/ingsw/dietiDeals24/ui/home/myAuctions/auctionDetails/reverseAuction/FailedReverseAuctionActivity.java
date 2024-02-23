3a19048ff220a4b827465a10af92e667b9a8f7e9
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2024-02-23 16:08:06 +0100

    Frontend: modificato MyAuctionDetails
    Iniziata a scrivere il caso d'uso delle offerte fatte alle aste dell'utente

--- Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/myAuctions/auctionDetails/reverseAuction/FailedReverseAuctionActivity.java
+++ Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/ui/home/myAuctions/auctionDetails/reverseAuction/FailedReverseAuctionActivity.java
@@ -1,12 +1,128 @@
 package com.ingsw.dietiDeals24.ui.home.myAuctions.auctionDetails.reverseAuction;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.MutableLiveData;
+
+import com.ingsw.dietiDeals24.R;
+import com.ingsw.dietiDeals24.controller.ImageController;
+import com.ingsw.dietiDeals24.controller.MyAuctionDetailsController;
+import com.ingsw.dietiDeals24.model.ReverseAuction;
+import com.ingsw.dietiDeals24.model.enumeration.AuctionStatus;
+import com.ingsw.dietiDeals24.model.enumeration.AuctionType;
+import com.ingsw.dietiDeals24.model.enumeration.Category;
+import com.ingsw.dietiDeals24.model.enumeration.Wear;
+import com.ingsw.dietiDeals24.ui.home.HomeActivity;
+import com.ingsw.dietiDeals24.ui.home.createAuction.auctionHolder.AuctionHolder;
+import com.ingsw.dietiDeals24.ui.home.createAuction.fragments.generalAuctionAttributes.GeneralAuctionAttributesViewModel;
 import com.ingsw.dietiDeals24.ui.home.myAuctions.auctionDetails.AuctionDetailsActivity;
+import com.ingsw.dietiDeals24.ui.utility.ToastManager;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class FailedReverseAuctionActivity extends AuctionDetailsActivity {
+    private ReverseAuction auction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        auction = (ReverseAuction) MyAuctionDetailsController.getAuction();
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
+        setAuctionDetails();
+        if (auction.getImages() != null) {
+            bindImages(auction);
+        }
+    }
 
+    private void setAuctionDetails() {
+        scrollViewAuctionDetails.setBackground(AppCompatResources.getDrawable(this, R.color.brown));
+        auctionTypeTextViewAuctionDetails.setText(AuctionType.toItalianString(auction.getType()));
+        categoryTextViewAuctionDetails.setText(Category.toItalianString(auction.getCategory()));
+        titleTextViewAuctionDetails.setText(auction.getTitle());
+
+        auctionStatusTextViewAuctionDetails.setText(AuctionStatus.toItalianString(auction.getStatus()));
+        auctionStatusTextViewAuctionDetails.setTextColor(ContextCompat.getColor(this, R.color.yellow));
+        auctionStatusTextViewAuctionDetails.setStrokeColor(R.color.black);
+
+        wearTextViewAuctionDetails.setText(Wear.toItalianString(auction.getWear()));
+        descriptionTextViewAuctionDetails.setText(auction.getDescription());
+        priceTextViewAuctionDetails.setText("Offerta corrente: " + auction.getStartingPrice() + "€");
+        specificInformation1TextViewAuctionDetails.setText("Scade il: " + MyAuctionDetailsController.getFormattedExpirationDate(auction));
+        specificInformation2TextViewAuctionDetails.setVisibility(View.GONE);
+        setButtons();
+    }
+
+    private void setButtons() {
+        setRedButton();
+        setGreenButton();
+    }
+
+    private void setGreenButton() {
+        greenButton.setBackground(AppCompatResources.getDrawable(this, R.drawable.square_shape_green));
+        greenButton.setText("RILANCIA");
+        greenButton.setOnClickListener(v -> {
+            new AlertDialog.Builder(this)
+                    .setTitle("Conferma")
+                    .setMessage("Sei sicuro di voler rilanciare l'asta?")
+                    .setPositiveButton("Si", (dialog, which) -> {
+                        GeneralAuctionAttributesViewModel viewModel = GeneralAuctionAttributesViewModel.getInstance();
+                        AuctionHolder auctionHolder = new AuctionHolder(
+                                auction.getTitle(),
+                                new ArrayList<>(ImageController.convertImageListToUriList(auction.getImages(), getApplicationContext())),
+                                auction.getDescription(),
+                                auction.getWear(),
+                                auction.getCategory()
+                        );
+
+                        viewModel.setNewAuction(new MutableLiveData<>(auctionHolder));
+
+                        try {
+                            MyAuctionDetailsController.deleteAuction(auction.getIdAuction()).get();
+                        } catch (ExecutionException e) {
+                            ToastManager.showToast(getApplicationContext(), e.getCause().getMessage());
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        Intent intent = new Intent(this, HomeActivity.class);
+                        intent.putExtra("openSilent", "SilentAuctionAttributesFragment");
+                        startActivity(intent);
+                        finish();
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        });
+    }
+
+    private void setRedButton() {
+        redButton.setBackground(AppCompatResources.getDrawable(this, R.drawable.square_shape_red));
+        redButton.setText("CANCELLA L'ASTA");
+        redButton.setOnClickListener(v -> {
+            new AlertDialog.Builder(this)
+                    .setTitle("Conferma")
+                    .setMessage("Sei sicuro di voler cancellare l'asta?")
+                    .setPositiveButton("Si", (dialog, which) -> {
+                        try {
+                            MyAuctionDetailsController.deleteAuction(auction.getIdAuction()).get();
+                            finish();
+                        } catch (ExecutionException e) {
+                            ToastManager.showToast(getApplicationContext(), e.getCause().getMessage());
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        });
     }
 }

cf517a8b09bc01f61d742ed8cce71107b901bffe
emadonnarumma <ema.donnarumma@studenti.unina.it>
2024-02-28 16:01:37 +0100

    Frontend: adesso si può filtrare la ricerca aste

--- Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/controller/SearchAuctionsController.java
+++ Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/controller/SearchAuctionsController.java
@@ -7,9 +7,9 @@ import com.ingsw.dietiDeals24.model.DownwardAuction;
 import com.ingsw.dietiDeals24.model.Image;
 import com.ingsw.dietiDeals24.model.ReverseAuction;
 import com.ingsw.dietiDeals24.model.SilentAuction;
+import com.ingsw.dietiDeals24.model.enumeration.Category;
 import com.ingsw.dietiDeals24.network.RetroFitHolder;
 import com.ingsw.dietiDeals24.network.TokenHolder;
-import com.ingsw.dietiDeals24.network.dao.MyAuctionsDao;
 import com.ingsw.dietiDeals24.network.dao.SearchAuctionsDao;
 
 import java.io.IOException;
@@ -99,6 +99,286 @@ public class SearchAuctionsController implements RetroFitHolder {
             try {
                 SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
                 Response<List<DownwardAuction>> response = searchAuctionsDao.getAllDownwardAuctions(TokenHolder.getAuthToken()).execute();
+                List<DownwardAuction> auctions = response.body();
+
+                for (DownwardAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    downwardAuctions = auctions;
+                    return downwardAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<SilentAuction>> getAllSilentAuctionsByCategory(Category category) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<SilentAuction>> response = searchAuctionsDao.getAllSilentAuctionsByCategory(category, TokenHolder.getAuthToken()).execute();
+                List<SilentAuction> auctions = response.body();
+
+                for (SilentAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    silentAuctions = auctions;
+                    return silentAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<ReverseAuction>> getAllReverseAuctionsByCategory(Category category) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<ReverseAuction>> response = searchAuctionsDao.getAllReverseAuctionsByCategory(category, TokenHolder.getAuthToken()).execute();
+                List<ReverseAuction> auctions = response.body();
+
+                for (ReverseAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    reverseAuctions = auctions;
+                    return reverseAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<DownwardAuction>> getAllDownwardAuctionsByCategory(Category category) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<DownwardAuction>> response = searchAuctionsDao.getAllDownwardAuctionsByCategory(category, TokenHolder.getAuthToken()).execute();
+                List<DownwardAuction> auctions = response.body();
+
+                for (DownwardAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    downwardAuctions = auctions;
+                    return downwardAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<SilentAuction>> getAllSilentAuctionsByKeyword(String keyword) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<SilentAuction>> response = searchAuctionsDao.getAllSilentAuctionsByKeyword(keyword, TokenHolder.getAuthToken()).execute();
+                List<SilentAuction> auctions = response.body();
+
+                for (SilentAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    silentAuctions = auctions;
+                    return silentAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<ReverseAuction>> getAllReverseAuctionsByKeyword(String keyword) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<ReverseAuction>> response = searchAuctionsDao.getAllReverseAuctionsByKeyword(keyword, TokenHolder.getAuthToken()).execute();
+                List<ReverseAuction> auctions = response.body();
+
+                for (ReverseAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    reverseAuctions = auctions;
+                    return reverseAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<DownwardAuction>> getAllDownwardAuctionsByKeyword(String keyword) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<DownwardAuction>> response = searchAuctionsDao.getAllDownwardAuctionsByKeyword(keyword, TokenHolder.getAuthToken()).execute();
+                List<DownwardAuction> auctions = response.body();
+
+                for (DownwardAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    downwardAuctions = auctions;
+                    return downwardAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<SilentAuction>> getAllSilentAuctionsByKeywordAndCategory(String keyword, Category category) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<SilentAuction>> response = searchAuctionsDao.getAllSilentAuctionsByKeywordAndCategory(keyword, category,TokenHolder.getAuthToken()).execute();
+                List<SilentAuction> auctions = response.body();
+
+                for (SilentAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    silentAuctions = auctions;
+                    return silentAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<ReverseAuction>> getAllReverseAuctionsByKeywordAndCategory(String keyword, Category category) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<ReverseAuction>> response = searchAuctionsDao.getAllReverseAuctionsByKeywordAndCategory(keyword, category,TokenHolder.getAuthToken()).execute();
+                List<ReverseAuction> auctions = response.body();
+
+                for (ReverseAuction auction : auctions) {
+                    Response<List<Image>> imagesResponse = searchAuctionsDao.getAllAuctionImages(auction.getIdAuction(), TokenHolder.getAuthToken()).execute();
+                    if (imagesResponse.isSuccessful())
+                        auction.setImages(imagesResponse.body());
+                }
+
+                if (response.isSuccessful()) {
+
+                    reverseAuctions = auctions;
+                    return reverseAuctions;
+
+                } else if (response.code() == 403) {
+                    return new ArrayList<>();
+                }
+
+            } catch (IOException e) {
+                throw new ConnectionException("Errore di connessione");
+            }
+            return new ArrayList<>();
+
+        });
+    }
+
+    public static CompletableFuture<List<DownwardAuction>> getAllDownwardAuctionsByKeywordAndCategory(String keyword, Category category) {
+        return CompletableFuture.supplyAsync(() -> {
+
+            try {
+                SearchAuctionsDao searchAuctionsDao = retrofit.create(SearchAuctionsDao.class);
+                Response<List<DownwardAuction>> response = searchAuctionsDao.getAllDownwardAuctionsByKeywordAndCategory(keyword, category,TokenHolder.getAuthToken()).execute();
                 List<DownwardAuction> auctions = response.body();
 
                 for (DownwardAuction auction : auctions) {

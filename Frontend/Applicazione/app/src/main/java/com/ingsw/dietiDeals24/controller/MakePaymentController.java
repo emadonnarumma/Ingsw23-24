2492e79a1d3bc73f0bf44bca06b25a693a109966
emadonnarumma <ema.donnarumma@studenti.unina.it>
2024-03-22 15:05:46 +0100

    Frontend: aggiunto formstate a MakePayment

--- Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/controller/MakePaymentController.java
+++ Frontend/Applicazione/app/src/main/java/com/ingsw/dietiDeals24/controller/MakePaymentController.java
@@ -1,5 +1,12 @@
 package com.ingsw.dietiDeals24.controller;
 
+import android.content.Context;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.ingsw.dietiDeals24.R;
+import com.ingsw.dietiDeals24.controller.formstate.MakePaymentFormState;
+import com.ingsw.dietiDeals24.controller.formstate.ReverseAuctionAttributesFormState;
 import com.ingsw.dietiDeals24.exceptions.AuthenticationException;
 import com.ingsw.dietiDeals24.exceptions.ConnectionException;
 import com.ingsw.dietiDeals24.model.Auction;
@@ -16,6 +23,7 @@ import com.ingsw.dietiDeals24.network.dao.MakePaymentDao;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
@@ -24,9 +32,98 @@ import retrofit2.Response;
 
 public class MakePaymentController implements RetroFitHolder {
 
+    private static MutableLiveData<MakePaymentFormState> makePaymentFormState = new MutableLiveData<>();
+
     private static Auction auction;
 
     private static Bid bid;
+    private static CreditCard creditCard;
+
+    public static MutableLiveData<MakePaymentFormState> getMakePaymentFormState() {
+        return makePaymentFormState;
+    }
+
+
+
+    public static void makePaymentDataChanged(String cardCode, String name, String surname, String expirationDate, String cvv, Context context) {
+        if (!isCardCodeValid(cardCode)) {
+            makePaymentFormState.setValue(new MakePaymentFormState(context.getString(R.string.card_code_error), null, null, null, null));
+        } else if (!isNameValid(name)) {
+            makePaymentFormState.setValue(new MakePaymentFormState(null, context.getString(R.string.name_error), null, null, null));
+        } else if (!isSurnameValid(surname)) {
+            makePaymentFormState.setValue(new MakePaymentFormState(null, null, context.getString(R.string.surname_error), null, null));
+        } else if (!isExpirationDateValid(expirationDate)) {
+            makePaymentFormState.setValue(new MakePaymentFormState(null, null, null, context.getString(R.string.expiration_date_error), null));
+        } else if (!isCvvValid(cvv)) {
+            makePaymentFormState.setValue(new MakePaymentFormState(null, null, null, null, context.getString(R.string.cvv_error)));
+        } else {
+            makePaymentFormState.setValue(new MakePaymentFormState(true));
+        }
+    }
+
+    private static boolean isCvvValid(String cvv) {
+
+        return cvv.length() == 3;
+    }
+
+    private static boolean isExpirationDateValid(String expirationDate) {
+        if (expirationDate.length() != 7) {
+            return false;
+        }
+
+        String[] parts = expirationDate.split("/");
+        if (parts.length != 2) {
+            return false;
+        }
+
+        String monthPart = parts[0];
+        String yearPart = parts[1];
+
+        int month;
+        int year;
+
+        try {
+            month = Integer.parseInt(monthPart);
+            year = Integer.parseInt(yearPart);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        if (month < 1 || month > 12) {
+            return false;
+        }
+
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1; // Calendar.MONTH returns 0-11 for Jan-Dec
+
+        if (year < currentYear) {
+            return false;
+        } else if (year == currentYear && month < currentMonth) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isSurnameValid(String surname) {
+
+        int surnameLength = surname.length();
+
+        return surnameLength > 0 && surnameLength <= 20;
+    }
+
+    private static boolean isNameValid(String name) {
+
+        int nameLength = name.length();
+
+        return nameLength > 0 && nameLength <= 20;
+    }
+
+    private static boolean isCardCodeValid(String cardCode) {
+
+        return cardCode.length() == 19;
+    }
+
 
     public static Bid getBid() {
         return bid;
@@ -43,8 +140,6 @@ public class MakePaymentController implements RetroFitHolder {
     public static void setAuction(Auction auction) {
         MakePaymentController.auction = auction;
     }
-
-    private static CreditCard creditCard;
     public static CreditCard getCreditCard() {
         return creditCard;
     }
